916bd9c358d8251c4df3f2e66dd17792beefc109
Itz-SuleX5 <itzsulex5@gmail.com>
2025-09-28 19:11:19 -0700

    Feat: words seeder route was fixed.

--- backend/words/management/commands/import_words.py
+++ backend/words/management/commands/import_words.py
@@ -8,7 +8,7 @@ class Command(BaseCommand):
     help = 'Imports words from a JSON file into the Word model'
 
     def handle(self, *args, **options):
-        json_file_path = os.path.join(settings.BASE_DIR, 'backend', 'data', 'english_words_2000.json')
+        json_file_path = os.path.join(settings.BASE_DIR, 'data', 'english_words_2000.json')
         with open(json_file_path, 'r', encoding='utf-8') as file:
             words_data = json.load(file)
 
